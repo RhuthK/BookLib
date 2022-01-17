8fc62bc1ad94e857224f80698c228280dd408864
MetroAlien2033 <rhutuvaruni1998@gmail.com>
2022-01-17 22:21:24 +0530

    added author functions

--- routes/authors.js
+++ routes/authors.js
@@ -1,19 +1,52 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
+
 
 // All author routes
-router.get('/', (req, res)=>{
-    res.render('authors/index')
+router.get('/', async (req, res)=>{
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors, 
+            searchOptions:req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // new author route
 router.get('/new', (req, res)=>{
-    res.render('authors/new')
+    res.render('authors/new', {author: new Author()})
 })
 
 // create author
-router.post('/',(req,res) =>{
-    res.send('Create')
+router.post('/', async(req, res)=>{
+    const author = new Author({
+        name: req.body.name
+    })
+    try{
+        const newAuthor = await author.save()
+        res.redirect('authors')
+    }catch{
+        let locals = {errorMessage:'something went wrong'}
+        res.render('authors', locals)
+    }
+    // author.save((err, newAuthor)=>{
+    //     let locals = { errorMessage : `something went wrong` }
+    //     if(err){
+    //         res.render(`authors`, locals)
+    //     } 
+    //     else{
+    //         res.redirect('authors')
+    //     }
+    // })
 })
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const indexRouter = require('./routes/index')
+const bodyParser = require('body-parser')
+const authorRouter = require('./routes/authors')
+
 
 app.set('view engine' , 'ejs')
 app.set('views', __dirname + '/views')
@@ -11,19 +15,23 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
 const db = mongoose.connection 
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to mongoose'))
 
-const indexRouter = require('./routes/index')
 app.use('/', indexRouter)
-
-const authorRouter = require('./routes/authors')
 app.use('/authors', authorRouter)
+
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log('Running on port ', port)
 })
+
+
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
