48f11c1f6f253e4db8e659de482f6566ac4f33f6
MetroAlien2033 <rhutuvaruni1998@gmail.com>
2022-01-17 14:07:50 +0530

    reverted changes made in imports

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
-import express, { static } from 'express'
+const express = require('express')
 const app = express()
-import expressLayouts from 'express-ejs-layouts'
+const expressLayouts = require('express-ejs-layouts')
 
 app.set('view engine' , 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(static('public'))
+app.use(express.static('public'))
 
-import { connect, connection } from 'mongoose'
-connect(process.env.DATABASE_URL, {useNewUrlParser:true})
-const db = connection 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
+const db = mongoose.connection 
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to mongoose'))
 
-import indexRouter from './routes/index'
+const indexRouter = require('./routes/index')
 app.use('/', indexRouter)
 
 const port = process.env.PORT || 3000
