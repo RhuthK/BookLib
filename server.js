4fda06e33743927a31366f15a6c104eddf424179
MetroAlien2033 <rhutuvaruni1998@gmail.com>
2022-01-17 13:32:53 +0530

    added routes and index.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+
 app.set('view engine' , 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-
-app.get('/', (req,res)=>{
-    res.send('You are on the homepage')
-})
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
